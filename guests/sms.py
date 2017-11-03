21ffdd06c30a1d0803772eeca86797af980e41c2
EC2 Default User <ec2-user@ip-172-31-21-237.ap-south-1.compute.internal>
2017-11-03 00:50:12 +0000

    add twilio production settings

--- guests/sms.py
+++ guests/sms.py
@@ -1,16 +1,18 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC3eaae3e0e3447653dde8de37f385937e"
+#account_sid = "AC3eaae3e0e3447653dde8de37f385937e"
+account_sid="AC5ec8ed1c98a7d5192a25eafdc42ba485"
 # Your Auth Token from twilio.com/console
-auth_token = "901ed36b5ea2200b3de8fd911885769c"
+#auth_token = "901ed36b5ea2200b3de8fd911885769c"
+auth_token ="58f23b261e25b9cc772f048f8bb80041"
 twilio_number ="+16318237195"
 client = Client(account_sid, auth_token)
 
 def send_sms(to_number, message):
     client.messages.create(
-        to="+5132934561",
-        from_='+15005550006',
+        to=to_number,
+        from_=twilio_number,
         body=message)
 
     print(message)
