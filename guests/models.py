248df64ade8d751f1f9b7e0d84a62825befa0175
Siddharth <sidd.scope@gmail.com>
2017-12-08 21:23:47 -0800

    message text

--- guests/models.py
+++ guests/models.py
@@ -108,7 +108,7 @@ class Guest(models.Model):
     @property
     def whatsapp_message(self):
         WHATSAPP_PREFIX = 'https://api.whatsapp.com/send?phone={0}&text={1}'
-        INVITATION_SMS_TEMPLATE = 'Hi *_{0}_*. Siddharth and Shreya are getting hitched on 8th Jan. We would love to have you bless us with your presence. Kindly click on this link for your invitation here {1}.'
+        INVITATION_SMS_TEMPLATE = 'Hi *_{0}_*. Siddharth and Shreya are getting hitched on 8th Jan. We would love to have you bless us with your presence. Kindly click on this link for your invitation here {1}'
         invite_message = INVITATION_SMS_TEMPLATE.format(self.party.name, self.party.invitation_link)
         invite_message = invite_message.replace(" ", "%20")
         invite_message = invite_message.replace("&", "%26")
